abef64835d267960dc2f00eae16c69cc71857ffa
abd <abdalhababseh@outlook.com>
2023-12-19 00:56:23 +0300

    hh

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -28,7 +28,7 @@ class PostController extends Controller
     public function index()
     {
         try {
-            $posts = Post::with('user:user_id,username')->get();
+            $posts = Post::with('user:user_id,name')->get();
 
             return response()->json([
                 'posts' => $posts
