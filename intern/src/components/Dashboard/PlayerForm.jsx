5f4587fb15fddcae69112e691ef00f8a48af908d
abd <abdalhababseh@outlook.com>
2023-12-19 15:47:00 +0300

    abod admin/sidebar file

--- intern/src/components/Dashboard/PlayerForm.jsx
+++ intern/src/components/Dashboard/PlayerForm.jsx
@@ -114,6 +114,7 @@ const PlayerForm = ({ player, setShowModal, addPlayer, updatePlayer }) => {
               <img src={formData.image} alt="Preview" className="w-20 h-20 rounded-full mt-2" />
             )}
           </div>
+          
           <button
             type="submit"
             className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
