150e50802349366b398da33066f934c1d05aa300
ReemHasanA <reem.hasan98@yahoo.com>
2023-12-20 22:04:10 +0300

    .

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LikeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -69,6 +70,7 @@ Route::get('/pending-requests/{userId}', [FriendRequestController::class, 'selec
 Route::post('/send-friend-request', [FriendRequestController::class, 'sendingFriendRequest']);
 // // // // // // // // // //
 
+Route::resource('likes', LikeController::class);
 
 
 
